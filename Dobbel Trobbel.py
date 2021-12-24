703106211e69510c467b311646285ad619b11c6e
99061142 <99061142@mydavinci.nl>
2021-12-24 19:23:36 +0100

    Made the starting scoreboard with lists

--- Dobbel Trobbel.py
+++ Dobbel Trobbel.py
@@ -1,17 +1,31 @@
-blue_dice = (1,2,3,4,5,6) # Dice amounts
-white_dice = (1,1,1,2,2,3) # Dice amounts
+red_dice = (1,2,3,4,5,6) # Blue Dice amounts
+blue_dice = (1,1,1,2,2,3) # White Dice amounts
+
+
+chosen_red_dices = [-2, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '] # Stadard values for the red scoreboard
+chosen_blue_dices = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '-2'] # Stadard values for the blue scoreboard
+chosen_white_dices = [' ', ' ', ' ', ' ', ' '] # Stadard values for the white scoreboard
 
 
 # Scoreboard
 def scoreboard():
+    chosen_red_dices_str = [f"| {dice} |" for dice in chosen_red_dices] # Make a list of the red values
+    chosen_blue_dices_str = [f"| {dice} |" for dice in chosen_blue_dices] # Make a list of the blue values
+    chosen_white_dices_str = [f"| {dice} |" for dice in chosen_white_dices] # Make a list of the white values
+
+
     print(
-        "-----------------------------------------------------------------------",
-        f"| {-2} |  | {0} |  | {0} |  | {0}  |  | {0} |  | {0} |   | {0} |   | {0} |  | {0} |  | {0} |",
-        "-----------------------------------------------------------------------",
+        "---------------------------------------------------------------------",
+        f"{'  '.join(chosen_red_dices_str)}",
+        "---------------------------------------------------------------------",
         "\n",
-        "-----------------------------------------------------------------------",
-        f"| {0} |  | {0} |  | {0} |  | {0}  |  | {0} |  | {0} |   | {0} |   | {0} |  | {0} |  | {-2} |",
-        "-----------------------------------------------------------------------",
+        "---------------------------------------------------------------------",
+        f"{'  '.join(chosen_blue_dices_str)}",
+        "---------------------------------------------------------------------",
+        "\n",
+        "                  ---------------------------------",
+        f"                  {'  '.join(chosen_white_dices_str)}",
+        "                  ---------------------------------",
         sep="\n", end="\n"
     )
 
