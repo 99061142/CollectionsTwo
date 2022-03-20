5b068bbf4ea8614dac373be410607365f0f6db78
99061142 <99061142@mydavinci.nl>
2022-03-20 21:53:05 +0100

    Deleted the code to create the decks per player

--- ai-uno.py
+++ ai-uno.py
@@ -15,6 +15,14 @@ cards = {
     "draw-four": 4
 }
 
+card_points = {
+    "draw_two": 20,
+    "reverse": 20,
+    "skip": 20,
+    "wild": 50,
+    "draw_four": 50
+}
+
 
 # Get the amount of players
 def ask_player_amount() -> int:
@@ -38,60 +46,8 @@ def ask_player_amount() -> int:
 
 
 # Get random cards for every player
-def card_amount_per_player(player_amount:int) -> dict:
-    player_decks = {} # All the cards for the players
-    
-    for player_num in range(player_amount):
-        player_name = player_num + 1 # Player name
-        player_decks[player_name] = [] # Player cards
-
-        # Get 7 random cards
-        for _ in range(7):
-            key = choice(list(cards)) # Card color / special name
-    
-            key_value = cards[key] # Get all the cards with the specific color or speciality
-
-            # If the value inside the key is an number
-            if isinstance(key_value, int):
-                card_name = key # Card name thats get added to the player
-
-                cards[key] -= 1 # Decrease the amount of cards by 1
-
-                # If the amount of cards is 0
-                if not cards[key]:
-                    del cards[key] # Delete the specific key name for the card
-
-            # If the value inside the key is an list
-            elif isinstance(key_value, list):
-                card_number = choice(key_value) # Random number of the color
-
-                card_name = f"{key}_{card_number}" # Card name thats get added to the player
-
-                cards[key].remove(card_number) # Remove the number
-
-                # If the list with the numbers for the color are empty
-                if not len(key_value):
-                    del cards[key] # Delete the specific key name for the card
-            
-            # If the value inside the key is an dictionary
-            else:
-                color_key = choice(list(key_value)) # Get all the colors inside the head key
-
-                card_name = f"{color_key}_{key}" # Card name thats get added to the player
-
-                cards[key][color_key] -= 1 # Decrease the amount of cards by 1
-
-                # If the dictionary with the numbers for the color are empty
-                if not cards[key][color_key]:
-                    del cards[key][color_key] # Delete the color key
-
-                    # If all the colors are empty
-                    if not cards[key]:
-                        del cards[key] # Delete the specific key name for the card
-
-            player_decks[player_name].append(card_name) # Add the random chosen card to the player
-
-    return player_decks
+def create_player_decks(player_amount:int) -> dict:
+    pass
 
 
 
@@ -99,4 +55,4 @@ def card_amount_per_player(player_amount:int) -> dict:
 # When the program starts
 if __name__ == "__main__":
     player_amount = ask_player_amount() # Ask the amount of players
-    player_decks = card_amount_per_player(player_amount) # Get the decks for the player
+    player_decks = create_player_decks(player_amount) # Get the decks for the player
