ad9d45b83d121779c516d1e9c9e37a0156824fd2
99061142 <99061142@mydavinci.nl>
2022-03-20 22:37:39 +0100

    Function to pick the random cards for the user

--- ai-uno.py
+++ ai-uno.py
@@ -47,8 +47,48 @@ def ask_player_amount() -> int:
 
 # Get random cards for every player
 def create_player_decks(player_amount:int) -> dict:
-    pass
+    player_decks = {player_number: [] for player_number in list(range(1,player_amount+1))} # List of player cards
 
+    # Pick 7 cards for every player
+    for i in range(7):
+        for player in range(1, player_amount+1):
+            card_type = choice(list(cards.keys())) # Random card type
+            card_options = cards[card_type] # Options for the card type
+
+            # If the option is a number (card type)
+            if(isinstance(card_options, int)): 
+                cards[card_type] -= 1 # Pick 1 card out of the deck
+
+                card_name = card_type # Save the card name, to add it inside the deck of player cards 
+
+            # If the option is a list (card type + number)
+            elif isinstance(card_options, list):
+                card_number = choice(card_options)
+
+                cards[card_type].remove(card_number) # Pick 1 card out of the deck
+
+                card_name = f'{card_type}_{card_number}' # Save the card name, to add it inside the deck of player cards 
+
+            # If the option is a dict (card type + color + number)
+            elif isinstance(card_options, dict):
+                card_color = choice(list(card_options.keys()))
+
+                cards[card_type][card_color] -= 1 # Pick 1 card out of the deck
+
+                card_name = f'{card_type}_{card_color}' # Save the card name, to add it inside the deck of player cards 
+
+                # If every card is used for the color of the card type
+                if(not cards[card_type][card_color]):
+                    del cards[card_type][card_color] # Delete the color for the card type
+
+
+            # If every card is used for the card type
+            if(not cards[card_type]):
+                del cards[card_type] # Delete the card type
+
+            player_decks[player].append(card_name) # Add the card name to the deck of the specific user
+
+    return player_decks
 
 
 
